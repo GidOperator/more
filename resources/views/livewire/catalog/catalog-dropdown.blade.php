76e7e0c160510a46738c531644ca24e01bf93344
Dima <olimpkemerovo@gmail.com>
2025-12-15 22:10:30 +0700

    feat: переделал каталог

--- resources/views/livewire/catalog/catalog-dropdown.blade.php
+++ resources/views/livewire/catalog/catalog-dropdown.blade.php
@@ -1,42 +1,50 @@
-<div class="position-relative" x-data="{ open: false }">
+<div class="position-relative" x-data="{ open: false, activeCategory: null }">
     <!-- Кнопка Каталог -->
     <button @click="open = !open" class="btn btn-primary text-dark">
         Каталог
     </button>
 
-    <!-- Большое меню -->
-    <div x-show="open" x-transition @click.away="open = false"
-        class="position-absolute start-0 bg-white shadow overflow-auto"
+    <!-- Mega Menu -->
+    <div x-show="open" x-transition @click.away="open = false" class="position-absolute bg-white shadow overflow-auto"
         style="top: 50px; width: 90vw; height: 50vh; background-color: rgba(255, 255, 255, 0.95); z-index: 1050; padding: 20px; color: black;">
-        <ul class="list-unstyled mb-0 row">
-            @foreach ($categories as $category)
-                <li class="col-4 mb-3" x-data="{ expanded: true }">
-                    <!-- Категория -->
-                    <div class="d-flex justify-content-between align-items-center p-2 border rounded"
-                        style="cursor: pointer; color: black;"
-                        @click="expanded = !expanded; $wire.set('selectedCategory', {{ $category->id }}); $wire.set('selectedSubcategory', null)">
-                        <span>{{ $category->name }}</span>
-                        @if (count($category->subcategories))
-                            <span :class="{ 'rotate-90': expanded }"
-                                style="display:inline-block; transition: transform 0.2s;">▶</span>
-                        @endif
-                    </div>
+        <div class="row h-100">
+            <!-- Слева: Основные категории -->
+            <div class="col-4 border-end h-100 overflow-auto">
+                <ul class="list-unstyled mb-0">
+                    @foreach ($categories as $category)
+                        <li class="mb-2">
+                            <div class="p-2 rounded" :class="{ 'bg-light': activeCategory === {{ $category->id }} }"
+                                style="cursor: pointer;"
+                                @mouseenter="activeCategory = {{ $category->id }}; $wire.set('selectedCategory', {{ $category->id }}); $wire.set('selectedSubcategory', null)">
+                                {{ $category->name }}
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
 
-                    <!-- Подкатегории -->
-                    @if (count($category->subcategories))
-                        <ul x-show="expanded" x-transition class="list-unstyled ms-3 mt-1">
-                            @foreach ($category->subcategories as $sub)
-                                <li class="p-1 rounded" style="cursor: pointer;"
-                                    @click="$wire.set('selectedSubcategory', {{ $sub->id }})"
+            <!-- Справа: Подкатегории выбранной категории -->
+            <div class="col-8 h-100 overflow-auto">
+                <ul class="list-unstyled mb-0">
+                    @if ($categories)
+                        @php
+                            $selected = $categories->firstWhere('id', $selectedCategory);
+                        @endphp
+                        @if ($selected && $selected->subcategories)
+                            @foreach ($selected->subcategories as $sub)
+                                <li class="p-2 rounded" style="cursor: pointer;"
                                     @mouseover="this.style.backgroundColor='#f0f0f0'"
-                                    @mouseout="this.style.backgroundColor='transparent'">
-                                    └─ {{ $sub->name }}
+                                    @mouseout="this.style.backgroundColor='transparent'"
+                                    wire:click="$set('selectedSubcategory', {{ $sub->id }})">
+                                    {{ $sub->name }}
                                 </li>
                             @endforeach
-                        </ul>
+                        @else
+                            <li class="p-2 text-muted">Выберите категорию слева</li>
+                        @endif
                     @endif
-                </li>
-            @endforeach
-        </ul>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
